2f07e686a46fd423e141a10ce14bf28f452c3d2e
Harrison <harrisonchambela2016@gmail.com>
2024-05-06 16:20:37 +0300

    This adds the activity execution methods

--- src/main/java/Money/Transfer/MoneyTransferWorkflowImpl.java
+++ src/main/java/Money/Transfer/MoneyTransferWorkflowImpl.java
@@ -1,6 +1,15 @@
 package Money.Transfer;
 
+import io.temporal.activity.ActivityOptions;
+import io.temporal.workflow.Workflow;
+import io.temporal.common.RetryOptions;
+
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+import java.time.Duration;
+import java.util.HashMap;
 
 public class MoneyTransferWorkflowImpl {
     private static final String WITHDRAW = "withdraw";
@@ -12,4 +21,24 @@ public class MoneyTransferWorkflowImpl {
             .setBackoffCoefficient(2)
             .setMaximumAttempts(500)
             .build();
+
+    private final ActivityOptions defaultActivityOptions = ActivityOptions.newBuilder()
+            // Specifying timeout options
+            .setStartToCloseTimeout(Duration.ofSeconds(5))
+            .setRetryOptions(retryOptions)
+            .build();
+
+    // Map
+    private final Map<String, ActivityOptions> perActivityMethodOptions = new HashMap<String, ActivityOptions>(){{
+        put(WITHDRAW, ActivityOptions.newBuilder().setHeartbeatTimeout(Duration.ofSeconds(5)).build());
+    }};
+    private final AccountActivity account = Workflow.newActivityStub(AccountActivity.class, defaultActivityOptions, perActivityMethodOptions);
+
+    // Activity method executions can be orchestrated here or from within other Activity methods.
+    @Override
+    public void transfer(String fromAccountId, String toAccountId, String referenceId, double amount) {
+
+        account.withdraw(fromAccountId, referenceId, amount);
+        account.deposit(toAccountId, referenceId, amount);
+    }
 }
